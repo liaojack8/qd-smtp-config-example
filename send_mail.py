af8d1500ad26052d80c42f47e706ae6359573e65
liaojack8 <liaojack8.tw@gmail.com>
2024-02-12 02:37:56 +0800

    simplify

--- send_mail.py
+++ send_mail.py
@@ -12,25 +12,31 @@ def _send_mail(to, subject, text=None, subtype='html'):
     msg['To'] = to
     try:
         print(f'send mail to {to}')
-        if config.mail_port:
-            if config.mail_ssl == 'True' or config.mail_port == 465: # SSL
+        if config.mail_ssl or config.mail_starttls or config.mail_port:
+            # SSL與STARTTLS皆被設定為強制使用, 衝突時, 優先使用SSL
+            tls = False
+            if config.mail_ssl == 'True': # 強制使用SSL
                 s = smtplib.SMTP_SSL(config.mail_smtp, config.mail_port)
-                s.connect(config.mail_smtp, config.mail_port)
-            elif config.mail_starttls == 'True' or config.mail_port == 587: # STARTTLS
+            elif config.mail_starttls == 'True': # 強制使用STARTTLS
                 s = smtplib.SMTP(config.mail_smtp, config.mail_port)
-                s.connect(config.mail_smtp, config.mail_port)
+                tls = True
+            elif config.mail_port == 465: # 透過埠號識別加密協議, 465多為SSL
+                s = smtplib.SMTP_SSL(config.mail_smtp, config.mail_port)
+            elif config.mail_port == 587: # 透過埠號識別加密協議, 587多為STARTTLS
+                s = smtplib.SMTP(config.mail_smtp, config.mail_port)
+                tls = True
+            s.connect(config.mail_smtp, config.mail_port)
+            if tls == True:
                 try:
                     smtp_tls_response = s.starttls()
                     print(f'SMTP TLS ==>: {smtp_tls_response}')
                 except smtplib.SMTPException as e:
                     print(f"smtp starttls failed: {e}")
-        else:
-            if config.mail_ssl:
-                s = smtplib.SMTP_SSL(config.mail_smtp)
-            else:
+
+        elif config.mail_ssl == 'False' and config.mail_starttls == 'False': # 無安全性傳輸
                 s = smtplib.SMTP(config.mail_smtp)
-            s.connect(config.mail_smtp)
-        # s = config.mail_ssl and smtplib.SMTP_SSL(config.mail_smtp) or smtplib.SMTP(config.mail_smtp)
+                s.connect(config.mail_smtp, config.mail_port)
+
         if config.mail_user:
             s.login(config.mail_user, config.mail_password)
         status = s.sendmail(config.mail_from, to, msg.as_string())
