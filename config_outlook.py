0b3dd57be4590045b878d303967e7c4585f5cfcf
liaojack8 <liaojack8.tw@gmail.com>
2024-02-11 23:48:38 +0800

    support outlook starttls

---
+++ config_outlook.py
@@ -0,0 +1,9 @@
+mail_smtp = "smtp-mail.outlook.com"
+mail_port = 587
+mail_ssl = 'False'
+mail_starttls = 'True' 
+mail_user = 'username@outlook.com'
+mail_password = 'password'
+mail_from = mail_user
+
+mail_service = 'outlook'
