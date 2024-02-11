028847da3000db55cd96a00cf2c2d1c0b26da0f9
liaojack8 <liaojack8.tw@gmail.com>
2024-02-11 23:47:26 +0800

    support gmail ssl and starttls

---
+++ config_gmail_ssl.py
@@ -0,0 +1,9 @@
+mail_smtp = "smtp.gmail.com"
+mail_port = 465
+mail_ssl = 'True'
+mail_starttls = 'False'
+mail_user = 'username@gmail.com'
+mail_password = 'password'
+mail_from = mail_user
+
+mail_service = 'gmail_ssl'
---
+++ config_gmail_starttls.py
@@ -0,0 +1,9 @@
+mail_smtp = "smtp.gmail.com"
+mail_port = 587
+mail_ssl = 'False'
+mail_starttls = 'True'
+mail_user = 'username@gmail.com'
+mail_password = 'password'
+mail_from = mail_user
+
+mail_service = 'gmail_starttls'
