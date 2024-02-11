f317678747631974367f92c823fcf7664b14954e
liaojack8 <liaojack8.tw@gmail.com>
2024-02-11 23:47:57 +0800

    support yahoo mail ssl and starttls

---
+++ config_yahoo_ssl.py
@@ -0,0 +1,9 @@
+mail_smtp = "smtp.mail.yahoo.com"
+mail_port = 465
+mail_ssl = 'True'
+mail_starttls = 'False'
+mail_user = 'username@myyahoo.com'
+mail_password = 'password'
+mail_from = mail_user
+
+mail_service = 'yahoo_ssl'
---
+++ config_yahoo_starttls.py
@@ -0,0 +1,9 @@
+mail_smtp = "smtp.mail.yahoo.com"
+mail_port = 587
+mail_ssl = 'False'
+mail_starttls = 'True'
+mail_user = 'username@myyahoo.com'
+mail_password = 'password'
+mail_from = mail_user
+
+mail_service = 'yahoo_starttls'
