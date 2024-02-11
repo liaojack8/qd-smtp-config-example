2e4bd5c448debc4c23917ba302f36a0d97cbc439
liaojack8 <liaojack8.tw@gmail.com>
2024-02-11 23:46:45 +0800

    create send_mail template

---
+++ send_mail.py
@@ -0,0 +1,44 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def _send_mail(to, subject, text=None, subtype='html'):
+    print(f"----Config: {config.mail_service}----")
+    if not config.mail_smtp:
+        print('no smtp')
+        return
+    msg = MIMEText(text, _subtype=subtype, _charset='utf-8')
+    msg['Subject'] = subject
+    msg['From'] = config.mail_from
+    msg['To'] = to
+    try:
+        print(f'send mail to {to}')
+        if config.mail_port:
+            if config.mail_ssl == 'True' or config.mail_port == 465: # SSL
+                s = smtplib.SMTP_SSL(config.mail_smtp, config.mail_port)
+                s.connect(config.mail_smtp, config.mail_port)
+            elif config.mail_starttls == 'True' or config.mail_port == 587: # STARTTLS
+                s = smtplib.SMTP(config.mail_smtp, config.mail_port)
+                s.connect(config.mail_smtp, config.mail_port)
+                try:
+                    smtp_tls_response = s.starttls()
+                    print(f'SMTP TLS ==>: {smtp_tls_response}')
+                except smtplib.SMTPException as e:
+                    print(f"smtp starttls failed: {e}")
+        else:
+            if config.mail_ssl:
+                s = smtplib.SMTP_SSL(config.mail_smtp)
+            else:
+                s = smtplib.SMTP(config.mail_smtp)
+            s.connect(config.mail_smtp)
+        # s = config.mail_ssl and smtplib.SMTP_SSL(config.mail_smtp) or smtplib.SMTP(config.mail_smtp)
+        if config.mail_user:
+            s.login(config.mail_user, config.mail_password)
+        status = s.sendmail(config.mail_from, to, msg.as_string())
+        s.close()
+        if not status:
+            print('寄信成功')
+        else:
+            print(f'寄信失敗。{status}')
+    except Exception as e:
+        print(f'send mail error: {e}')
+    return
